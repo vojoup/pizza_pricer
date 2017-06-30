90c6916d086138561681a713d1981dbd00757b05
Vojta Oupicky <voj.oup@gmail.com>
2017-06-30 09:46:43 +0200

    A program for computing the cheapest pizza
    The program outputs the cheapest pizza according to the input values the user gives it.

---
+++ pizza.py
@@ -0,0 +1,28 @@
+def cena_jednotky_pizzy(prumery, ceny) :
+	vysledky = []
+	for pizza in range(0, pocet_pizz) :
+		p = prumery[pizza]
+		c = ceny[pizza]
+		vysledky.append(c/p)
+	return vysledky
+
+def nejlevnejsi_pizza(vysledne_ceny) :
+	for cena in vysledne_ceny :
+		
+
+pocet_pizz = int(input("Kolik ruznych cen pizzy chces porovnavat?\n"))
+pizzy_prumer = []
+pizzy_cena = []
+cena_za_jednotku = []
+for pizza in range(0, pocet_pizz) :
+	#print(pizza)
+	pizzy_prumer.append(int(input("Jaky prumer ma pizza " + str(pizza + 1) + '?\n')))
+	pizzy_cena.append(int(input("Kolik stoji pizza " + str(pizza + 1) + '?\n')))
+
+"""for pizza in range(0, pocet_pizz) :
+	p = pizzy_prumer[pizza]
+	c = pizzy_cena[pizza]
+	vysledek = c / p 
+	print('Pizza %s stoji %s kc za jednotku.' %((pizza + 1), vysledek))
+"""
+ceny_pizz_za_jednotku = cena_jednotky_pizzy(pizzy_prumer, pizzy_cena)
