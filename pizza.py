cc6bc748abf6dbcae3313c27d62647102b30e83f
Vojta Oupicky <voj.oup@gmail.com>
2017-07-02 22:16:28 +0200

    More complite version of OOP version
    Incorporated the comparison feature for the cheapest pizza.

--- pizza.py
+++ pizza.py
@@ -1,28 +1,30 @@
-def cena_jednotky_pizzy(prumery, ceny) :
-	vysledky = []
-	for pizza in range(0, pocet_pizz) :
-		p = prumery[pizza]
-		c = ceny[pizza]
-		vysledky.append(c/p)
-	return vysledky
+class Pizza(object):
+	def __init__(self, prumer, cena):
+		self.prumer = prumer
+		self.cena = cena
+	def displayPizza(self):
+		print('Prumer : %s\nCena : %s\nCena za jednotku : %s\n' %(self.prumer, self.cena, self.cenaZaJednotku()))
+	def cenaZaJednotku(self):
+		cena = float(self.cena) / float(self.prumer)
+		return cena
 
-def nejlevnejsi_pizza(vysledne_ceny) :
-	return vysledne_ceny.index(min(vysledne_ceny))
+def display_result(ceny, pizzy):
+	i = ceny.index(min(ceny))
+	print('Nejlevnejsi je pizza cislo %s.\n' %(i + 1))
+	print('Stoji %s kc za %s cm prumeru.\n' %(pizzy[i].cena, pizzy[i].prumer))
 
-pocet_pizz = int(input("Kolik ruznych cen pizzy chces porovnavat?\n"))
-pizzy_prumer = []
-pizzy_cena = []
-cena_za_jednotku = []
-for pizza in range(0, pocet_pizz) :
-	#print(pizza)
-	pizzy_prumer.append(int(input("Jaky prumer ma pizza " + str(pizza + 1) + '?\n')))
-	pizzy_cena.append(int(input("Kolik stoji pizza " + str(pizza + 1) + '?\n')))
-
-"""for pizza in range(0, pocet_pizz) :
-	p = pizzy_prumer[pizza]
-	c = pizzy_cena[pizza]
-	vysledek = c / p 
-	print('Pizza %s stoji %s kc za jednotku.' %((pizza + 1), vysledek))
-"""
-ceny_pizz_za_jednotku = cena_jednotky_pizzy(pizzy_prumer, pizzy_cena)
-print('Nejlevnejsi je pizza cislo %s.' %(nejlevnejsi_pizza(ceny_pizz_za_jednotku) + 1))
+pizzy = []
+ceny_za_jednotku = []
+pocet_pizz = int(input('Kolik pizz si prejes porovnat?\n'))
+for pizza in range(pocet_pizz):
+	prumer = input('Jaky ma prumer pizza cislo %s ?\n' %(pizza + 1))
+	cena = input('Jaka je cena pizzy cislo %s ?\n' %(pizza + 1))
+	pizzy.append(Pizza(prumer, cena))
+print('')
+for i in range(pocet_pizz):
+	print('Pizza cislo %s' %(i + 1))
+	pizzy[i].displayPizza()
+for i in range(pocet_pizz):
+	ceny_za_jednotku.append(pizzy[i].cenaZaJednotku())
+#print(min(ceny_za_jednotku))
+display_result(ceny_za_jednotku, pizzy)
--- pokus_oop_pizza.py
+++ pokus_oop_pizza.py
@@ -3,18 +3,28 @@ class Pizza(object):
 		self.prumer = prumer
 		self.cena = cena
 	def displayPizza(self):
-		print('Prumer : %s\nCena : %s\n' %(self.prumer, self.cena))
+		print('Prumer : %s\nCena : %s\nCena za jednotku : %s\n' %(self.prumer, self.cena, self.cenaZaJednotku()))
 	def cenaZaJednotku(self):
 		cena = float(self.cena) / float(self.prumer)
 		return cena
 
+def display_result(ceny, pizzy):
+	i = ceny.index(min(ceny))
+	print('Nejlevnejsi je pizza cislo %s.\n' %(i + 1))
+	print('Stoji %s kc za %s cm prumeru.\n' %(pizzy[i].cena, pizzy[i].prumer))
+
 pizzy = []
+ceny_za_jednotku = []
 pocet_pizz = int(input('Kolik pizz si prejes porovnat?\n'))
 for pizza in range(pocet_pizz):
-	prumer = input('Jaky ma prumer pizza cislo 1 ?\n')
-	cena = input('Jaka je cena pizzy cislo 1 ?\n')
+	prumer = input('Jaky ma prumer pizza cislo %s ?\n' %(pizza + 1))
+	cena = input('Jaka je cena pizzy cislo %s ?\n' %(pizza + 1))
 	pizzy.append(Pizza(prumer, cena))
 print('')
 for i in range(pocet_pizz):
 	print('Pizza cislo %s' %(i + 1))
-	pizzy[i].displayPizza()	
+	pizzy[i].displayPizza()
+for i in range(pocet_pizz):
+	ceny_za_jednotku.append(pizzy[i].cenaZaJednotku())
+#print(min(ceny_za_jednotku))
+display_result(ceny_za_jednotku, pizzy)
